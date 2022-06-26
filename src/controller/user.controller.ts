7efa0507b6c1a7d7a3dde182946c412a8e56e50d
Samir Mishra <smir.mishra1551@gmail.com>
2022-06-27 00:48:31 +0545

    user controller bug fixed

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -7,12 +7,13 @@ export class UserController {
     async  getOneUser(req: Request, res: Response){
         try{
             const userDetails = req.body;
-            if(!userDetails.email) return res.status(404).json({message: "User doesnot exist"})
+
             const user = await User.findOne({
                 where: {
                     email: userDetails.email
                 }
             })
+            if(!user) return res.status(404).json({message: "User doesnot exist"})
             res.status(200).json(
                 {
                     data: user
