5d690a03f741bacb0dcdba9fa8fcfc01d83f16f2
Samir Mishra <smir.mishra1551@gmail.com>
2022-07-09 20:46:16 +0545

    some minor changes

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,30 +1,50 @@
 import { Request, Response } from "express";
-import User  from "./index"
+import { User }  from "../entity/User"
+// import bcrypt from 'bcrypt'
 
 
 export class UserController {
+    async  getUser(req: Request, res: Response){
+        const { email } = req.body
 
-    async  getOneUser(req: Request, res: Response){
         try{
-            const userDetails = req.body;
-
-            const user = await User.findOne({
-                where: {
-                    email: userDetails.email
-                }
+            const user = await User.findOne({ where: { email: email } });
+            if (!user) {
+                return res.status(200).json({ error: 'User Not Found' });
+            }
+            return res.status(200).json({
+                data: user
             })
-            if(!user) return res.status(404).json({message: "User doesnot exist"})
-            res.status(200).json(
-                {
-                    data: user
-                }
-            )
         }catch (e) {
-            res.status(500).json({
-                message: e.message
-            })
+            console.log(e)
+            return res.status(500)
         }
 
+    }
+
+    async createUser(req: Request, res: Response){
+        const userData = req.body
+        try{
+            // const hashedPassword = bcrypt.genSalt(10, userData.password)
+            // const token = Math.floor(Math.random() * 4)
+            const userPasswordd = "this is trial"
+            const user = await User.create({
+                fName: userData.firstname,
+                lName: userData.lastname,
+                email: userData.email,
+                password: userPasswordd,
+                // verification_token: token,
+
+
+            })
+            return res.status(201).json({
+                data: user
+            })
+
+        }catch (e) {
+            console.log(e)
+            return res.status(500)
+        }
     }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
+import { UserController } from './controller/user.controller';
 import 'reflect-metadata';
 
 export const app = express();
 
 (async () => {
+
     app.use(cors());
     app.use(cookieParser());
     app.use(express.json())
+    const userController = new UserController();
 
+    app.get('/user',userController.getUser )
+    app.post('/user/create', userController.createUser)
 
 })();
